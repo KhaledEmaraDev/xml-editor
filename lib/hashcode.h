f53ff7851b6a2dffa4b7e7a5d1de2672c2f59f5d
Hassan El-shazly <hassanelshazly2010@gmail.com>
2021-07-14 05:11:43 +0200

    Add Documentation to MPair & HashCode and Fix bugs in MPair

--- lib/hashcode.h
+++ lib/hashcode.h
@@ -11,8 +11,16 @@ static const size_t prime = 1000000007;
 static const size_t a = 17;
 static const size_t b = 7;
 
+
 template <class key_t,
           std::enable_if_t<std::is_integral<key_t>::value, int> = 0>
+/**
+ * @brief hash_code
+ *        A templete universal hash function for integral types
+ *        this function is enabled only if the key_t is an integral type
+ *
+ * @complexity O(1)
+ */
 size_t hash_code(key_t key) {
     size_t hash = (a * key + b) % prime;
     return hash;
@@ -22,6 +30,14 @@ template <class key_t,
           std::enable_if_t<std::is_same<key_t, QString>::value ||
                            std::is_same<key_t, std::string>::value ||
                            std::is_same<key_t, const char *>::value, int> = 0>
+/**
+ * @brief hash_code
+ *        A templete universal hash function for strings
+ *        this function is enabled only if the key_t is string type
+ *        (const char *, std::string, QString)
+ *
+ * @complexity O(length of the string)
+ */
 size_t hash_code(key_t key) {
     int length = 0;
     if constexpr(std::is_same<key_t, const char *>::value)
--- lib/mpair.h
+++ lib/mpair.h
@@ -6,63 +6,122 @@
 #include <QDebug>
 
 template <typename key_t, typename value_t>
+/**
+ * @brief The MPair struct
+ *        Map pair is a key/value pair used in HashMap
+ */
 struct MPair {
     const key_t key;
     value_t value;
 
+    /**
+     * @brief MPair
+     *        default constructor
+     */
     MPair() = default;
 
+    /**
+     * @brief MPair
+     *        construct MPair from key and the defaut value
+     */
     MPair(const key_t& key)
         : key(key), value(value_t()) {}
 
+    /**
+     * @brief MPair
+     *        construct MPair from key and value
+     */
     MPair(const key_t& key, const value_t& value)
         : key(key), value(value) {}
 
+    /**
+     * @brief MPair
+     *        copy constructor
+     */
     MPair(const MPair& mp)
         : key(mp.key), value(mp.value) {}
 
+    /**
+     * @brief MPair
+     *        move constructor
+     */
     MPair(MPair&& mp)
         : key(std::move(mp.key)),
           value(std::move(mp.value)) {}
 
+    /**
+     * @brief MPair
+     *        copy constructor from std::pair
+     */
+    MPair(const std::pair<key_t, value_t>& sp)
+        : key(sp.first), value(sp.second) {}
+
+    /**
+     * @brief MPair
+     *        move constructor from std::pair
+     */
     MPair(std::pair<key_t, value_t>&& sp)
         : key(std::move(sp.first)),
           value(std::move(sp.second)) {}
 
-    MPair(const std::pair<key_t, value_t>& sp)
-        : key(std::move(sp.first)),
-          value(std::move(sp.second)) {}
+    /**
+     * @brief MPair
+     *        copy constructor from QPair
+     */
+    MPair(const QPair<key_t, value_t>& qp)
+        : key(qp.first), value(qp.second) {}
 
+    /**
+     * @brief MPair
+     *        move constructor from QPair
+     */
     MPair(QPair<key_t, value_t>&& qp)
         : key(std::move(qp.first)),
           value(std::move(qp.second)) {}
 
-    MPair(const QPair<key_t, value_t>& qp)
-        : key(std::move(qp.first)),
-          value(std::move(qp.second)) {}
-
+    /**
+     * @brief operator =
+     *        assignment operator
+     * @return reference to allow chaning
+     */
     MPair& operator=(const MPair& mp)
     {
         key = mp.key;
         value = mp.value;
+        return *this;
     }
 
+    /**
+     * @brief operator =
+     *        move assignment operator
+     * @return reference to allow chaning
+     */
     MPair& operator=(MPair&& mp)
     {
         key = std::move(mp.key);
         value = std::move(mp.value);
+        return *this;
     }
 
+    /**
+     * @brief operator ==
+     */
     bool operator==(const MPair& mp)
     {
         return key == mp.key && value == mp.value;
     }
 
+    /**
+     * @brief operator !=
+     */
     bool operator!=(const MPair& mp)
     {
         return key != mp.key || value != mp.value;
     }
 
+    /**
+     * @brief operator <
+     */
     bool operator<(const MPair& mp)
     {
         if(key == mp.key)
@@ -70,11 +129,17 @@ struct MPair {
         return key < mp.key;
     }
 
+    /**
+     * @brief operator <=
+     */
     bool operator<=(const MPair& mp)
     {
         return key <= mp.key;
     }
 
+    /**
+     * @brief operator >
+     */
     bool operator>(const MPair& mp)
     {
         if(key == mp.key)
@@ -82,11 +147,17 @@ struct MPair {
         return key > mp.key;;
     }
 
+    /**
+     * @brief operator >=
+     */
     bool operator>=(const MPair& mp)
     {
         return key >= mp.key;
     }
 
+    /**
+     * @brief swap
+     */
     void swap(const MPair& mp)
     {
         std::swap(key, mp.key);
@@ -95,6 +166,12 @@ struct MPair {
 };
 
 template <typename key_t, typename value_t>
+/**
+ * @brief make_mpair
+ * @param key
+ * @param value
+ * @return
+ */
 MPair<key_t, value_t>
 make_mpair(const key_t& key, const value_t& value)
 {
@@ -102,6 +179,11 @@ make_mpair(const key_t& key, const value_t& value)
 }
 
 template <typename key_t, typename value_t>
+/**
+ * @brief operator <<
+ *        overload QDebug to enable debuging of MPair
+ * @return
+ */
 QDebug
 operator<<(QDebug dbg, const MPair<key_t, value_t> &mp)
 {
